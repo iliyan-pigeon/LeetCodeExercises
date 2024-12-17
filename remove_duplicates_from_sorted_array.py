a6fd1918a1989d00bb82ae898e2f1cfd354a0048
Iliyan Galabov <milenov556@gmail.com>
2024-12-17 13:42:05 +0200

    Update remove_duplicates_from_sorted_array.py

--- remove_duplicates_from_sorted_array.py
+++ remove_duplicates_from_sorted_array.py
@@ -1,1 +1,18 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
 
+        while True:
+            control = []
+
+            for i in nums:
+                if i in control:
+                    nums.remove(i)
+                    break
+                else:
+                    control.append(i)
+
+            if len(control) == len(nums):
+                break
+
+        return len(nums)        
+                
