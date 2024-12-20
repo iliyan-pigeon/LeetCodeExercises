e5bb37c0c7afdeb54914ddf49e539e69468742af
Iliyan Galabov <milenov556@gmail.com>
2024-12-20 09:10:17 +0200

    Update majoriry_element.py

--- majoriry_element.py
+++ majoriry_element.py
@@ -1,1 +1,10 @@
-
+class Solution(object):
+    def majorityElement(self, nums):
+    
+        elements = set(nums)
+    
+        for element in elements:
+            count = nums.count(element)
+    
+            if count > len(nums) / 2:
+                return element
