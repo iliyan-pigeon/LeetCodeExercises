c75e1c0cfc89efaebcc7bebf554db6c3ac58b4e7
Iliyan Galabov <milenov556@gmail.com>
2025-01-04 18:26:14 +0200

    Update rotate_array.py

--- rotate_array.py
+++ rotate_array.py
@@ -1,3 +1,14 @@
+# Solution 1
+class Solution(object):
+    def rotate(self, nums, k):
+        n = len(nums)
+        k %= n
+
+        nums.reverse()
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
+        
+# Solution 2
 class Solution(object):
     def rotate(self, nums, k):
         for i in range(k):
