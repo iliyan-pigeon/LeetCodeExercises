279412837d01b0e036684f6fc59adad0b5b7c230
Iliyan Galabov <milenov556@gmail.com>
2025-01-04 11:27:02 +0200

    Update rotate_array.py

--- rotate_array.py
+++ rotate_array.py
@@ -1,14 +1,6 @@
-from collections import deque
-
-
 class Solution(object):
     def rotate(self, nums, k):
-        nums_queue = deque(nums)
         for i in range(k):
-            number = nums_queue.pop()
-            nums_queue.appendleft(number)
-
-        nums = list(nums_queue)    
-
-        return nums
-
+            if nums:
+                number = nums.pop()
+                nums.insert(0, number)
