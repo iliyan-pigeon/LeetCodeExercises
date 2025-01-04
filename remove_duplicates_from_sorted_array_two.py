16b69a0a36ecd3b984c4a03aa1ac589d56eca784
Iliyan Galabov <milenov556@gmail.com>
2025-01-04 20:58:39 +0200

    Update remove_duplicates_from_sorted_array_two.py

--- remove_duplicates_from_sorted_array_two.py
+++ remove_duplicates_from_sorted_array_two.py
@@ -1,1 +1,5 @@
-
+class Solution(object):
+    def removeDuplicates(self, nums):
+        for i in range(len(nums) - 1, -1, -1):
+            if nums.count(nums[i]) > 2:
+                nums.pop(i)
