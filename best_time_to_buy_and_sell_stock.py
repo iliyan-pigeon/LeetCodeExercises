51a2b5eb5290808337110dd3c8839b9d734e6eec
Iliyan Galabov <milenov556@gmail.com>
2025-01-06 10:50:24 +0200

    Update best_time_to_buy_and_sell_stock.py

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -1,7 +1,14 @@
+import sys
+
+
 class Solution(object):
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        
+        biggest_distance = - sys.maxsize
+        for i in range(len(prices)):
+            for j in range(i, len(prices)):
+                current_distance = prices[j] - prices[i]
+
+                if biggest_distance < current_distance:
+                    biggest_distance = current_distance
+
+        return biggest_distance
