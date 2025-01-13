cebd13caa29ca9c0f97ff24bc29753fb879e6aa3
Iliyan Galabov <milenov556@gmail.com>
2025-01-13 16:10:45 +0200

    Update insert_delete_get_random.py

--- insert_delete_get_random.py
+++ insert_delete_get_random.py
@@ -1,3 +1,4 @@
+# Solution 1
 import random
 
 
@@ -23,3 +24,39 @@ class RandomizedSet(object):
     def getRandom(self):
         
         return random.choice(self.set)
+
+
+# Solution 2
+import random
+
+
+class RandomizedSet:
+    def __init__(self):
+        self.dict = {}  
+        self.list = [] 
+
+    def insert(self, val):
+        if val in self.dict:
+            return False  
+        self.dict[val] = len(self.list)  
+        self.list.append(val)  
+        return True
+
+    def remove(self, val):
+        if val not in self.dict:
+            return False  
+        
+        index_to_remove = self.dict[val]
+        last_element = self.list[-1] 
+
+        
+        self.list[index_to_remove] = last_element
+        self.dict[last_element] = index_to_remove
+
+        
+        self.list.pop()
+        del self.dict[val]
+        return True
+
+    def getRandom(self):
+        return random.choice(self.list)
