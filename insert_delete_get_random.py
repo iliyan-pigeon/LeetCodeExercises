23078e3818200f5f23e6bf39032c69b394ee1e0c
Iliyan Galabov <milenov556@gmail.com>
2025-01-13 16:00:15 +0200

    Update insert_delete_get_random.py

--- insert_delete_get_random.py
+++ insert_delete_get_random.py
@@ -1,1 +1,25 @@
+import random
 
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.set = []
+
+    def insert(self, val):
+        if val not in self.set:
+            self.set.append(val)
+            return True
+
+        return False
+
+    def remove(self, val):
+        if val in self.set:
+            self.set.remove(val)
+            return True
+
+        return False
+
+    def getRandom(self):
+        
+        return random.choice(self.set)
