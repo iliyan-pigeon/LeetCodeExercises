b216da1f5c6aade64d3024a29cf2341a7e0898a4
Iliyan Galabov <milenov556@gmail.com>
2025-01-13 10:15:48 +0200

    Update h_index.py

--- h_index.py
+++ h_index.py
@@ -1,7 +1,14 @@
 class Solution(object):
     def hIndex(self, citations):
-        """
-        :type citations: List[int]
-        :rtype: int
-        """
-        
+        h_index = 0
+
+        for i in range(len(citations) - 1, -1, -1):
+            count = len([j for j in citations if j >= citations[i]])
+
+            if citations[i] >= count > h_index:
+                h_index = count
+            else:
+                if h_index < citations[i] and count >= citations[i]:
+                    h_index = citations[i]
+
+        return h_index
