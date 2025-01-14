60fdb5cef8d62cc589896d8f785d990ba714c619
Iliyan Galabov <milenov556@gmail.com>
2025-01-14 16:04:46 +0200

    Update gas_station.py

--- gas_station.py
+++ gas_station.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution(object):
     def canCompleteCircuit(self, gas, cost):
         n = len(gas)
@@ -14,3 +15,34 @@ class Solution(object):
                 current_tank = 0
 
         return start_index if total_tank >= 0 else -1
+
+
+# Solution 2
+class Solution(object):
+    def canCompleteCircuit(gas, cost):
+        result = -1
+        gas_tank = 0
+        start_index = 0
+        full_circle = False
+
+        while start_index < len(gas) and full_circle is not True:
+
+            for i in range(len(gas)):
+                gas_tank += gas[0]
+                gas_tank -= cost[0]
+                gas.append(gas.pop(0))
+                cost.append(cost.pop(0))
+
+                if gas_tank < 0:
+                    gas_tank = 0
+                    break
+                elif i == len(gas) - 1:
+                    full_circle = True
+
+            if full_circle is not True:
+                start_index += 1
+
+        if full_circle:
+            result = start_index
+
+        return result
