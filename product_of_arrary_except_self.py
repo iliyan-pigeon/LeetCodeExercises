feb53cf8b3c466a78bf9f18d31457a6c0fa568fc
Iliyan Galabov <milenov556@gmail.com>
2025-01-14 11:24:24 +0200

    Update product_of_arrary_except_self.py

--- product_of_arrary_except_self.py
+++ product_of_arrary_except_self.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution(object):
     def productExceptSelf(self, nums):
         answer = []
@@ -8,5 +9,24 @@ class Solution(object):
             for j in current_nums:
                 result *= j
             answer.append(result)
+
+        return answer
+
+
+# Solution 2
+class Solution(object):
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        answer = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            answer[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= suffix
+            suffix *= nums[i]
 
         return answer
