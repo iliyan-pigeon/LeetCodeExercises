090fe975a8a8bf8eb22a5a8d2687590217193416
Iliyan Galabov <milenov556@gmail.com>
2025-01-15 13:14:32 +0200

    Update trapping_rain_water.py

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -1,1 +1,17 @@
+class Solution(object):
+    def trap(self, height):
+        water = 0
+        highest = 0
 
+        for i, current_height in enumerate(height):
+            if current_height >= highest and [h for index, h in enumerate(height[i:]) if index != 0 and h >=    current_height]:
+                highest = current_height
+            elif not [h for index, h in enumerate(height[i:]) if index != 0 and h >= current_height]:
+                for h in sorted(height[i:], reverse=True):
+                    if h < current_height:
+                        highest = h
+                        break
+            elif current_height < highest and i < len(height) - 1:
+                water += highest - current_height
+
+        return water
