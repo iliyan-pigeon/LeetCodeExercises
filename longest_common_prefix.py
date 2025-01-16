f4f2b65c2bdd65b601cab06f11a75f8439aff6ae
Iliyan Galabov <milenov556@gmail.com>
2025-01-16 11:23:19 +0200

    Update longest_common_prefix.py

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,1 +1,13 @@
+def longestCommonPrefix(strs):
+    prefix = ""
+    words_amount = len(strs)
+    min_length_word = min([len(i) for i in strs])
 
+    for i in range(min_length_word, -1, -1):
+        current_prefixes = [strs[j][:i] for j in range(words_amount)]
+
+        if len(set(current_prefixes)) == 1:
+            prefix = current_prefixes[0]
+            break
+
+    return prefix
