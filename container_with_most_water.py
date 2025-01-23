c21bf89415aa7a011aa55ee731587efd5ab6189e
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-01-23 15:36:47 +0200

    update

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution(object):
     def maxArea(self, height):
         result = 0
@@ -9,3 +10,22 @@ class Solution(object):
                     result = current
 
         return result
+
+
+#Solution 2
+class Solution:
+    def maxArea(self, height):
+        left, right = 0, len(height) - 1
+        max_area = 0
+
+        while left < right:
+            current_area = min(height[left], height[right]) * (right - left)
+            max_area = max(max_area, current_area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
