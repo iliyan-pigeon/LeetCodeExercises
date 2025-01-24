649600eea149b89c1fb3691739d258eb5280215d
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-01-24 15:12:14 +0200

    update

--- minimum_size_subarray_sum.py
+++ minimum_size_subarray_sum.py
@@ -2,25 +2,25 @@ import sys
 
 
 # Solution 1
-#def minSubArrayLen(target, nums):
-#    counter = 1
-#    minimal_subarray = sys.maxsize
-#
-#    while counter <= len(nums):
-#
-#        for i in range(len(nums)):
-#            sum_numbers = sum(nums[i:i+counter])
-#            if sum_numbers >= target:
-#                minimal_subarray = counter
-#                counter = len(nums)
-#                break
-#
-#        counter += 1
-#
-#    if minimal_subarray != sys.maxsize:
-#        return minimal_subarray
-#
-#    return 0
+def minSubArrayLen(target, nums):
+    counter = 1
+    minimal_subarray = sys.maxsize
+
+    while counter <= len(nums):
+
+        for i in range(len(nums)):
+            sum_numbers = sum(nums[i:i+counter])
+            if sum_numbers >= target:
+                minimal_subarray = counter
+                counter = len(nums)
+                break
+
+        counter += 1
+
+    if minimal_subarray != sys.maxsize:
+        return minimal_subarray
+
+    return 0
 
 
 # Solution 2
