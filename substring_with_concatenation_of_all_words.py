668fa3f52b793cff555664a7557c7a9d1acdd21c
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-01-28 17:04:20 +0200

    update

--- substring_with_concatenation_of_all_words.py
+++ substring_with_concatenation_of_all_words.py
@@ -1,3 +1,4 @@
+# Solution 1
 def findSubstring(s, words):
     word_length = len(words[0])
     all_words_length = len("".join(words))
@@ -16,3 +17,42 @@ def findSubstring(s, words):
             starting_points.append(i)
 
     return starting_points
+
+
+# Solution 2
+from collections import Counter
+
+class Solution(object):
+    def findSubstring(self, s, words):
+        if not s or not words:
+            return []
+
+        word_length = len(words[0])
+        word_count = len(words)
+        substring_length = word_length * word_count
+        word_frequency = Counter(words)
+        result = []
+
+        for i in range(word_length):
+            left = i
+            right = i
+            current_count = Counter()
+            while right + word_length <= len(s):
+
+                word = s[right:right + word_length]
+                right += word_length
+                if word in word_frequency:
+                    current_count[word] += 1
+
+                    while current_count[word] > word_frequency[word]:
+                        current_count[s[left:left + word_length]] -= 1
+                        left += word_length
+
+                    if right - left == substring_length:
+                        result.append(left)
+                else:
+
+                    current_count.clear()
+                    left = right
+        return result
+
