28dd8e65ec6b54f13b3db758658b8298b9a83e09
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-01-30 15:11:31 +0200

    update

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -1,3 +1,4 @@
+# Solution 1
 def isValidSudoku(board):
     left = 0
     right = 3
@@ -32,3 +33,30 @@ def isValidSudoku(board):
             right += 3
 
     return result
+
+
+# Solution 2
+class Solution(object):
+    def isValidSudoku(self, board):
+
+        for i in range(9):
+            row = [num for num in board[i] if num != '.']
+            if len(row) != len(set(row)):
+                return False
+
+            column = [board[j][i] for j in range(9) if board[j][i] != '.']
+            if len(column) != len(set(column)):
+                return False
+
+        for box_row in range(0, 9, 3):
+            for box_col in range(0, 9, 3):
+                sub_box = []
+                for i in range(box_row, box_row + 3):
+                    for j in range(box_col, box_col + 3):
+                        num = board[i][j]
+                        if num != '.':
+                            sub_box.append(num)
+                if len(sub_box) != len(set(sub_box)):
+                    return False
+
+        return True
