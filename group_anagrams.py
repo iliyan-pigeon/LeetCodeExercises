a480b9042b6203c7ddaec6cdf0dfb968da276b2e
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-02-05 14:52:15 +0200

    update

--- group_anagrams.py
+++ group_anagrams.py
@@ -1,15 +1,17 @@
-def groupAnagrams(strs):
-    anagrams = [[]]
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagrams = [[]]
 
-    for word in strs:
-        word_added = False
+        for word in strs:
+            word_added = False
 
-        for anagram in anagrams:
-            if not anagram or sorted(word) == sorted(anagram[0]):
-                anagram.append(word)
-                word_added = True
+            for anagram in anagrams:
+                if not anagram or sorted(word) == sorted(anagram[0]):
+                    anagram.append(word)
+                    word_added = True
 
-        if not word_added:
-            anagrams.append([word])
+            if not word_added:
+                anagrams.append([word])
 
-    return anagrams
+        return anagrams
+    
