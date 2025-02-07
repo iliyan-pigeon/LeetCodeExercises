acce316317a24d46210a11f37b9acc7ac6291c41
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-02-07 16:44:25 +0200

    update

--- min_stack.py
+++ min_stack.py
@@ -1,3 +1,4 @@
+# Solution 1
 class MinStack(object):
 
     def __init__(self):
@@ -14,3 +15,27 @@ class MinStack(object):
 
     def getMin(self):
         return min(self.object)
+
+
+# Solution 2
+class MinStack(object):
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self):
+        if self.stack:
+            top = self.stack.pop()
+            if top == self.min_stack[-1]:
+                self.min_stack.pop()
+
+    def top(self):
+        return self.stack[-1] if self.stack else None
+
+    def getMin(self):
+        return self.min_stack[-1] if self.min_stack else None
