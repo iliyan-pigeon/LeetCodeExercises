ebeb9167339398c6d3c7fb10042e36665c199dd8
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-02-14 14:42:45 +0200

    update

---
+++ reverse_nodes_in_k_group.py
@@ -0,0 +1,13 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        dummy = ListNode()
+        dummy.next = head
+        prev = dummy
+
+
