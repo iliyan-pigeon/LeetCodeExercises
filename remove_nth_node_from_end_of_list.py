fd655e4343a63fde71ae039bdf4a007cdbb98a8d
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-02-17 11:57:36 +0200

    update

---
+++ remove_nth_node_from_end_of_list.py
@@ -0,0 +1,23 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
+        first = second = dummy
+
+        for _ in range(n + 1):
+            first = first.next
+
+        while first:
+            first = first.next
+            second = second.next
+
+        second.next = second.next.next
+
+        return dummy.next
+    
