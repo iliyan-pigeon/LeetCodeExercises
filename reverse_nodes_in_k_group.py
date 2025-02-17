de03544cad9b62a6948189758c14f87bf530df26
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-02-17 10:01:18 +0200

    update

--- reverse_nodes_in_k_group.py
+++ reverse_nodes_in_k_group.py
@@ -10,4 +10,22 @@ class Solution(object):
         dummy.next = head
         prev = dummy
 
+        nodes_amount = 0
+        reversed_nodes = 0
 
+        while prev.next is not None:
+            nodes_amount += 1
+            prev = prev.next
+
+        current = prev.next
+        reversed_nodes += k
+        while reversed_nodes < nodes_amount:
+            for _ in range(k-1):
+                next_node = current.next
+                current.next = next_node.next
+                next_node.next = prev.next
+                prev.next = next_node
+
+                reversed_nodes += k
+
+        return dummy.next
