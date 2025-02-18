a45b47a71856eea28da8917c307eba24dfd6b087
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-02-18 14:29:47 +0200

    update

--- maximum_depth_of_binary_three.py
+++ maximum_depth_of_binary_three.py
@@ -5,9 +5,33 @@ class TreeNode:
         self.right = right
 
 
+# Solution 1
 class Solution:
     def maxDepth(self, root):
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-    
+
+
+# Solution 2
+from collections import deque
+
+
+class Solution:
+    def maxDepth(self,root):
+        if not root:
+            return 0
+
+        queue = deque([root])
+        depth = 0
+
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()  # Correct BFS processing
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return depth
