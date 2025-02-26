850b3073fb6488d576962cfead9672c92c7fa02a
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-02-26 10:16:51 +0200

    update

---
+++ partition_list.py
@@ -0,0 +1,24 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def partition(self, head, x):
+        before_head = before = ListNode(0)
+        after_head = after = ListNode(0)
+
+        while head:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+            head = head.next
+
+        after.next = None
+        before.next = after_head.next
+
+        return before_head.next
