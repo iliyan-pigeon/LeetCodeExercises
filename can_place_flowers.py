6d8b567e8f54948a9b345f89c5db000e072a67eb
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-02-28 13:58:44 +0200

    update

--- can_place_flowers.py
+++ can_place_flowers.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         possible_plants = 0
@@ -18,3 +19,16 @@ class Solution:
         if n <= possible_plants:
             return True
         return False
+
+
+# Solution 2
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        currentflowers = sum(flowerbed)
+        flowerbed.insert(0,0)
+        flowerbed.append(0) # adding two zeros at the begining and the end will not change the result
+        for j in range(1,len(flowerbed)-1):
+            if (flowerbed[j] == 0) and (flowerbed[j-1] == 0) and (flowerbed[j+1] == 0):
+                flowerbed[j] = 1
+        maxflowers = sum(flowerbed)
+        return currentflowers+n <= maxflowers
