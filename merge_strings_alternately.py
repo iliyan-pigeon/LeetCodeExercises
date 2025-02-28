3e553f56cd19e0fd304284e4a05cc703ff0900ed
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-02-28 10:12:15 +0200

    update

--- merge_strings_alternately.py
+++ merge_strings_alternately.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution(object):
     def mergeAlternately(self, word1, word2):
         combined = ""
@@ -11,3 +12,25 @@ class Solution(object):
                 combined += word2[i]
 
         return combined
+
+
+# Solution 2
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        word3 = []
+        n1 = len(word1)
+        n2 = len(word2)
+
+        if n1 == n2:
+            for i in range(n1):
+                word3.append(word1[i]+word2[i])
+        elif n1 > n2:
+            for i in range(n2):
+                word3.append(word1[i]+word2[i])
+            word3.append(word1[(i+1):])
+        else:
+            for i in range(n1):
+                word3.append(word1[i]+word2[i])
+            word3.append(word2[(i+1):])
+
+        return "".join(word3)
