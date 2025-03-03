eb968e9a5ae6a57c79759f13eed81080a0f77ed1
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-03-03 10:29:20 +0200

    update

--- reverse_vowels_of_a_string.py
+++ reverse_vowels_of_a_string.py
@@ -1,3 +1,14 @@
-class Solution:
-    def reverseVowels(self, s: str) -> str:
-        pass
+def reverseVowels(s: str) -> str:
+    vowels_list = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
+    current_vowels = []
+    for i in range(len(s)):
+        if s[i] in vowels_list:
+            current_vowels.append(s[i])
+
+    for i in range(len(s)):
+        if s[i] in vowels_list:
+            s = s[:i] + current_vowels.pop() + s[i+1:]
+    return s
+
+
+print(reverseVowels("IceCreAm"))
