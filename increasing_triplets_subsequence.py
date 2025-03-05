10855f7a7788c0268686985af468c2896b6be09e
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-03-05 10:52:55 +0200

    update

--- increasing_triplets_subsequence.py
+++ increasing_triplets_subsequence.py
@@ -1,28 +1,26 @@
 from typing import List
 
 
-def increasingTriplet(nums: List[int]) -> bool:
-    result = False
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        result = False
 
-    for i in range(len(nums)-2):
-        current_number = nums[i]
-
-        for j in range(i+1, len(nums)-1):
-            if nums[j] > current_number:
-                current_number = nums[j]
-
-                for k in range(j+1, len(nums)):
-                    if nums[k] > current_number:
-                        result = True
-                        break
-            if result is True:
-                break
+        for i in range(len(nums)-2):
             current_number = nums[i]
 
-        if result is True:
-            break
+            for j in range(i+1, len(nums)-1):
+                if nums[j] > current_number:
+                    current_number = nums[j]
 
-    return result
+                    for k in range(j+1, len(nums)):
+                        if nums[k] > current_number:
+                            result = True
+                            break
+                if result is True:
+                    break
+                current_number = nums[i]
 
+            if result is True:
+                break
 
-print(increasingTriplet([1,5,0,4,1,3]))
+        return result
