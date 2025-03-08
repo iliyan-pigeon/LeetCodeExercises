ea82ded0967e225a3a8df744d32da16c329b3d45
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-03-08 13:33:00 +0200

    update

--- move_zeroes.py
+++ move_zeroes.py
@@ -3,6 +3,23 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+
+        while True:
+            should_break = False
+
+            for i in range(len(nums)):
+                if nums[i:].count(0) == len(nums[i:]):
+                    should_break = True
+                    break
+
+                if nums[i] == 0:
+                    nums.pop(i)
+                    nums.append(0)
+                    break
+
+            if should_break:
+                break
+
+
+a = Solution()
+print(a.moveZeroes([0,1,0,3,12]))
