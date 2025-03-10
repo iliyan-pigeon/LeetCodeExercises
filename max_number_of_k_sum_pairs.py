caf56e46ffa505e25ef45fb195030b55cf914669
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-03-10 15:57:17 +0200

    update

--- max_number_of_k_sum_pairs.py
+++ max_number_of_k_sum_pairs.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# Solution 1
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
 
@@ -14,6 +15,26 @@ class Solution:
 
         return len(pairs_amount) // 2
 
+
+# Solution 2
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        left, right = 0, len(nums)-1
+        count = 0
+
+        nums.sort()
+
+        while left != right:
+            if nums[left] + nums[right] == k:
+                count += 1
+                left += 1
+                right -= 1
+            elif nums[left] + nums[right] < k:
+                left += 1
+            else:
+                right -= 1
+
+        return count
 
 
 a = Solution()
