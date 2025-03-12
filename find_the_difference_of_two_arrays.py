03660911133b4334affae45624dfbbe9c01437a3
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-03-12 15:52:37 +0200

    update

--- find_the_difference_of_two_arrays.py
+++ find_the_difference_of_two_arrays.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# Solution 1
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
 
@@ -15,3 +16,22 @@ class Solution:
                 result[1].append(integer)
 
         return result
+
+
+# Solution 2
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        list1 = list(set1-set2)
+        list2 = list(set2-set1)
+
+        return [list1, list2]
+
+
+# Solution 3
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1, set2 = set(nums1), set(nums2)
+        return [[*(set1 - set2)], [*(set2 - set1)]]
