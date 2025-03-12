7871593e88d57844a603aa0e8d5e671a70ad3fa0
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-03-12 14:49:14 +0200

    update

--- find_pivot_index.py
+++ find_pivot_index.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# Solution 1
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
 
@@ -9,4 +10,18 @@ class Solution:
                 return i
 
         return -1
-    
+
+
+# Solution 2
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total_sum = sum(nums)
+        left_sum = 0
+
+        for i in range(len(nums)):
+            right_sum = total_sum - left_sum - nums[i]
+            if left_sum == right_sum:
+                return i
+            left_sum += nums[i]
+
+        return -1
