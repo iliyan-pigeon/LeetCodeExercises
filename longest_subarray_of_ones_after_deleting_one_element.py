35337447be77e8883a49120d90907505768277ad
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-03-12 11:08:19 +0200

    update

--- longest_subarray_of_ones_after_deleting_one_element.py
+++ longest_subarray_of_ones_after_deleting_one_element.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# Solution 1
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         k = 1
@@ -17,3 +18,30 @@ class Solution:
 
                 start += 1
         return end - start + 1
+
+
+# Solution 2
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        k = 1
+        longest_run = 0
+        start = 0
+        zeros_count = 0
+
+        for end in range(len(nums)+1):
+            current_binary = nums[start:end]
+
+            if len(current_binary) > longest_run:
+                longest_run = len(current_binary)
+
+            if end < len(nums):
+                if nums[end] == 0:
+                    zeros_count += 1
+
+            while zeros_count > k:
+                if nums[start] == 0:
+                    zeros_count -= 1
+
+                start += 1
+
+        return longest_run
