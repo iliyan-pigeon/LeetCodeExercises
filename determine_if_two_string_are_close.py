5d8554c910513a1285a9fd5ded1b08de7436a65f
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-03-13 13:08:24 +0200

    update

--- determine_if_two_string_are_close.py
+++ determine_if_two_string_are_close.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 
+# Solution 1
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2):
@@ -16,3 +17,21 @@ class Solution:
             count2[c] += 1
 
         return count1.keys() == count2.keys() and (sorted(count1.values()) == sorted(count2.values()))
+
+
+# Solution 2
+from collections import Counter
+
+
+class Solution:
+    def closeStrings(self, word1, word2):
+        if len(word1) != len(word2):
+            return False
+
+        if set(word1) != set(word2):
+            return False
+
+        freq1 = Counter(word1)
+        freq2 = Counter(word2)
+
+        return sorted(freq1.values()) == sorted(freq2.values())
