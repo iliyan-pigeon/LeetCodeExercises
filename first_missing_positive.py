441c75a69c0ab21c2fd3c08fb3c3dfea9345d399
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-03-30 13:44:30 +0300

    update

--- first_missing_positive.py
+++ first_missing_positive.py
@@ -1,7 +1,10 @@
 from typing import List
 
 
+# Solution 1
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        pass
-    
+
+        for i in range(1, max(nums)+2):
+            if i not in nums:
+                return i
