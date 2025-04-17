87b6439d41d3ea5acd94a238b1b396826db4d082
Iliyan Galabov <milenov556@gmail.com>
2025-04-17 10:52:41 +0300

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -1,6 +1,29 @@
 from typing import List
 
 
+# Solution 1
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, path):
+            # Append the current subset to the result
+            result.append(path[:])
+            length_nums = len(nums)
+            for i in range(start, length_nums):
+                # Include nums[i] in the subset
+                path.append(nums[i])
+                # Move to the next element
+                current = backtrack(i + 1, path)
+                # Exclude nums[i] from the subset and backtrack
+                path.pop()
+
+            return path[:]
+
+        result = []
+        backtrack(0, [])
+        return result
+
+
+# Solution 2
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
