f7991454936cf0540149506c42331b2f2fc007cf
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-04-27 10:28:53 +0300

    update

--- intersection_of_two_arrays_two.py
+++ intersection_of_two_arrays_two.py
@@ -3,4 +3,14 @@ from typing import List
 
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pass
+        result = []
+
+        for number in set(nums1):
+            if number in nums2:
+                times = min(nums1.count(number), nums2.count(number))
+
+                for i in range(times):
+                    result.append(number)
+
+        return result
+    
