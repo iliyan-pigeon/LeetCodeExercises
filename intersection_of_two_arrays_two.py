c69e8461ae45a547fe8f46ae5322b5d842d8af10
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-04-29 09:47:35 +0300

    update

--- intersection_of_two_arrays_two.py
+++ intersection_of_two_arrays_two.py
@@ -1,16 +1,51 @@
 from typing import List
 
+from collections import defaultdict
+
+
+# Solution 1
 
 class Solution:
+
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
         result = []
 
         for number in set(nums1):
+
             if number in nums2:
+
                 times = min(nums1.count(number), nums2.count(number))
 
                 for i in range(times):
                     result.append(number)
 
         return result
-    
+
+
+# Solution 2
+
+class Solution:
+
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        counter = defaultdict(int)
+
+        if len(nums1) < len(nums2):
+            nums1, nums2 = nums2, nums1
+
+        for i in nums1:
+            counter[i] += 1
+
+        result = []
+
+        for i in nums2:
+
+            counts = counter[i]
+
+            if counts > 0:
+                result.append(i)
+
+                counter[i] -= 1
+
+        return result
