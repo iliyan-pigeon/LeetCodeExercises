d725ad2a01161cd2b47ac197bca5e14bea8ceedc
Iliyan Galabov <milenov556@gmail.com>
2025-05-02 09:50:56 +0300

    Update longest_harmonious_subsequence.py

--- longest_harmonious_subsequence.py
+++ longest_harmonious_subsequence.py
@@ -3,4 +3,27 @@ from typing import List
 
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        pass
+
+        front = 0
+        back = 1
+
+        result = 0
+        start = nums[front]
+        nums.sort()
+
+        while front != len(nums)-1:
+            current = nums[front:back]
+            filtered = [i for i in current if i == start or i-1 == start]
+
+            if current == filtered and len(filtered) > result:
+                result = len(filtered)
+
+            elif len(current) != len(filtered) or back == len(nums):
+                front += 1
+                start = nums[front]
+
+            if back != len(nums):
+                back += 1
+
+        return result
+        
