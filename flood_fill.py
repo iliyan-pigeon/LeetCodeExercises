2cb2fa4e745f57e7228a2486b5f74222830f16ef
Iliyan Galabov <milenov556@gmail.com>
2025-05-05 14:37:27 +0300

    Create flood_fill.py

---
+++ flood_fill.py
@@ -0,0 +1,55 @@
+from typing import List
+
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        def change_colour(r, c, cl):
+            if image[r][c] == cl:
+                image[r][c] = color
+
+        color_for_change = image[sr][sc]
+        pixels_for_change = [[sr, sc]]
+        already_processed = []
+
+        while pixels_for_change:
+            p = pixels_for_change[0]
+            row = p[0]
+            column = p[1]
+
+            change_colour(row, column, color_for_change)
+            pixels_for_change.pop(0)
+
+            if row + 1 < len(image):
+                if image[row+1][column] == color_for_change and [row+1, column] not in already_processed:
+                    pixels_for_change.append([row+1, column])
+                    already_processed.append([row+1, column])
+                if column + 1 < len(image[row]) and [row+1, column+1] not in already_processed:
+                    pixels_for_change.append([row+1, column+1])
+                    already_processed.append([row+1, column+1])
+                if column - 1 >= 0 and [row+1, column-1] not in already_processed:
+                    pixels_for_change.append([row+1, column-1])
+                    already_processed.append([row+1, column-1])
+
+            if row - 1 >= 0:
+                if image[row - 1][column] == color_for_change and [row - 1, column] not in already_processed:
+                    pixels_for_change.append([row - 1, column])
+                    already_processed.append([row - 1, column])
+                if column + 1 < len(image[row]) and [row - 1, column + 1] not in already_processed:
+                    pixels_for_change.append([row - 1, column + 1])
+                    already_processed.append([row - 1, column + 1])
+                if column - 1 >= 0 and [row - 1, column - 1] not in already_processed:
+                    pixels_for_change.append([row - 1, column - 1])
+                    already_processed.append([row - 1, column - 1])
+
+            if column + 1 < len(image[row]):
+                if image[row][column + 1] == color_for_change and [row, column + 1] not in already_processed:
+                    pixels_for_change.append([row, column + 1])
+                    already_processed.append([row, column + 1])
+
+            if column - 1 >= 0:
+                if image[row][column - 1] == color_for_change and [row, column - 1] not in already_processed:
+                    pixels_for_change.append([row, column - 1])
+                    already_processed.append([row, column - 1])
+
+        return image
+      
