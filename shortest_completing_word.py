1e8eaf1acc8ac1558a3efa3763bcac7c675261e5
Iliyan Galabov <milenov556@gmail.com>
2025-05-07 15:39:21 +0300

    Update shortest_completing_word.py

--- shortest_completing_word.py
+++ shortest_completing_word.py
@@ -4,15 +4,19 @@ from typing import List
 class Solution:
     def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
 
+        filtered_plate = "".join([i for i in licensePlate if i.isalpha()]).lower()
+
         the_plate = ""
 
         for word in words:
-            for ch in licensePlate:
-                if ch.isalpha() and ch.lower() not in word.lower():
-                    continue
+            valid = True
+            for ch in filtered_plate:
+                if filtered_plate.count(ch) != word.lower().count(ch):
+                    valid = False
+                    break
 
-                if len(the_plate) > len(word) or not the_plate:
-                    the_plate = word
+            if valid and len(the_plate) > len(word) or valid and len(the_plate) == 0:
+                the_plate = word
 
         return the_plate
-      
+        
