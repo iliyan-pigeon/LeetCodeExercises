bdbbe59a61e8bfe5dce787491a8482190287bc30
Iliyan Galabov <milenov556@gmail.com>
2025-05-08 14:29:36 +0300

    Create most_common_word.py

---
+++ most_common_word.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        paragraph_list = [i.strip(",.!?") for i in paragraph.split(" ")]
+        occurrences = {}
+        result = ""
+        highest_amount = 0
+
+        for word in paragraph_list:
+            if word not in banned:
+                if word not in occurrences:
+                    occurrences[word] = 0
+                occurrences[word] += 1
+                if occurrences[word] > highest_amount:
+                    highest_amount = occurrences[word]
+                    result = word
+
+        return result
+      
