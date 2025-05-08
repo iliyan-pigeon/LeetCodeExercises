08b2e04e750306fdda3d00324433d6b9e87812d5
Iliyan Galabov <milenov556@gmail.com>
2025-05-08 16:00:45 +0300

    Create flipping_an_image.py

---
+++ flipping_an_image.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for i in range(len(image)):
+            image[i] = [image[i][a] for a in range(len(image[i])-1, -1, -1)]
+
+            for j in range(len(image[i])):
+                if image[i][j] == 1:
+                    image[i][j] = 0
+                else:
+                    image[i][j] = 1
+
+        return image
+      
