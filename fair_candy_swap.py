1f25312a0e049c8a8b46b5104d3389cb714bc051
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-05-09 14:14:07 +0300

    update

--- fair_candy_swap.py
+++ fair_candy_swap.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# Solution 1
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
         alice_total = sum(aliceSizes)
@@ -10,4 +11,17 @@ class Solution:
             for j in bobSizes:
                 if alice_total - i + j == bob_total + i - j:
                     return [i, j]
-                  
+
+
+# Solution 2
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        alice_total = sum(aliceSizes)
+        bob_total = sum(bobSizes)
+        delta = (alice_total - bob_total) // 2
+
+        bob_set = set(bobSizes)
+
+        for x in aliceSizes:
+            if x - delta in bob_set:
+                return [x, x - delta]
