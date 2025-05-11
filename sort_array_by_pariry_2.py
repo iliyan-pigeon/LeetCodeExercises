60d83f1558a61aa4be8eeb51d0a6cae4da518ea0
unknown <milenov556@gmail.com>
2025-05-11 11:23:55 +0300

    update

---
+++ sort_array_by_pariry_2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        reordered = []
+        even = 0
+        odd = 1
+
+        for i in nums:
+            if i % 2 == 0:
+                reordered.insert(even, i)
+                even += 2
+            else:
+                reordered.insert(odd, i)
+                odd += 2
+
+        return reordered
