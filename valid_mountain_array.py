110e54fc3712be473b5db00adc6d758411108fe6
Iliyan Galabov <milenov556@gmail.com>
2025-05-12 14:13:24 +0300

    Create valid_mountain_array.py

---
+++ valid_mountain_array.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+
+        result = True
+        direction = "Up"
+        current_number = -float("inf")
+
+        for number in arr:
+            if direction == "Up":
+                if number < current_number:
+                    direction = "Down"
+                elif number == current_number:
+                    result = False
+                    break
+
+            elif direction == "Down":
+                if number >= current_number:
+                    result = False
+                    break
+
+            current_number = number
+
+        return result
+      
