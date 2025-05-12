dcc17f2514bf8041b0a353a55c0a6a9b5389dcde
Iliyan Galabov <milenov556@gmail.com>
2025-05-12 11:49:19 +0300

    Create unique_email_addresses.py

---
+++ unique_email_addresses.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        unique_emails = []
+
+        for email in emails:
+            name, domain = email.split("@")
+            name = name.replace(".", "")
+
+            if "+" in name:
+                plus_index = name.index("+")
+                name = name[:plus_index]
+
+            the_email = name + domain
+
+            if the_email not in unique_emails:
+                unique_emails.append(email)
+
+        return len(unique_emails)
+      
