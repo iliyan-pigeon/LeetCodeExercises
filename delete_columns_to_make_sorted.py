ca06e2e45372f34d07cfd5e0f75beb101cc1d6ba
Iliyan Galabov <milenov556@gmail.com>
2025-05-13 16:43:34 +0300

    Create delete_columns_to_make_sorted.py

---
+++ delete_columns_to_make_sorted.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        result = 0
+        pointer = 0
+
+        while pointer < len(strs[0]):
+            previous = None
+            for word in strs:
+                if previous is None:
+                    previous = word[pointer]
+
+                elif ord(word[pointer]) <= ord(previous):
+                    result += 1
+                    break
+
+            pointer += 1
+
+        return result
+      
