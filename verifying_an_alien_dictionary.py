57f24ab62fcdd2a1488f133c9a6c8c62b5b4852b
unknown <milenov556@gmail.com>
2025-05-14 09:37:53 +0300

    update

--- verifying_an_alien_dictionary.py
+++ verifying_an_alien_dictionary.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# Solution 1
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         char_index_map = {char: index for index, char in enumerate(order)}
@@ -11,4 +12,23 @@ class Solution:
         sorted_words = sorted(words, key=alien_order)
 
         return words == sorted_words
-      
+
+
+# Solution 2
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        if len(words) == 1:
+            return True
+        d = {}
+        for i in range(len(order)):
+            d[order[i]] = i + 1
+
+        for i in range(len(words) -1):
+            for j in range(len(words[i])):
+                if j >= len(words[i+1]):
+                    return False
+                if words[i][j] != words[i+1][j]:
+                    if d[words[i][j]] > d[words[i+1][j]]:
+                        return False
+                    break
+        return True
