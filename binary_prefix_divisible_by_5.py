c2aa656d5d00a9dff3c258f0064f46998599e2d9
Iliyan Galabov <milenov556@gmail.com>
2025-05-15 16:25:28 +0300

    Update binary_prefix_divisible_by_5.py

--- binary_prefix_divisible_by_5.py
+++ binary_prefix_divisible_by_5.py
@@ -2,18 +2,14 @@ from typing import List
 
 
 class Solution:
-    def prefixesDivBy5(self, A: List[int]) -> List[bool]:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
         result = []
-        current_value = 0
+        current_value = ""
 
-        for bit in A:
+        for bit in nums:
+            current_value += str(bit)
 
-            print(bit)
-            print(current_value << 1)
-
-            current_value = (current_value << 1) | bit
-
-            if current_value % 5 == 0:
+            if int(current_value, 2) % 5 == 0:
                 result.append(True)
             else:
                 result.append(False)
