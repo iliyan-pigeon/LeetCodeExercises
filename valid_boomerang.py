8c32980270501de1d974e8cf221b449b5a3a8c77
unknown <milenov556@gmail.com>
2025-05-16 13:47:53 +0300

    update

--- valid_boomerang.py
+++ valid_boomerang.py
@@ -3,17 +3,15 @@ from typing import List
 
 class Solution:
     def isBoomerang(self, points: List[List[int]]) -> bool:
-        result = True
+        p1, p2, p3 = points
 
-        rows = []
-        cols = []
+        if p1 == p2 or p1 == p3 or p2 == p3:
+            return False
 
-        for i in range(len(points)):
-            rows.append(points[i][0])
-            cols.append(points[i][1])
+        x1, y1 = p1
+        x2, y2 = p2
+        x3, y3 = p3
 
-        if len(set(rows)) == 1 or len(set(cols)) == 1 or rows == cols:
-            result = False
-
-        return result
-      
+        return (x1 * (y2 - y3) +
+                x2 * (y3 - y1) +
+                x3 * (y1 - y2)) != 0
