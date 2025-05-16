6cc52b5582311205319ea9781c56c0eb9a778a4e
unknown <milenov556@gmail.com>
2025-05-16 15:28:52 +0300

    update

--- relative_sort_array.py
+++ relative_sort_array.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# Solution 1
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
 
@@ -19,4 +20,28 @@ class Solution:
         known.extend(sorted(unknown))
 
         return known
-      
+
+
+# Solution 2
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        d = {}
+        for arr in arr1:
+            if arr in d:
+                d[arr] += 1
+            else:
+                d[arr] = 1
+        res1 = []
+        res2 = []
+        for key in arr2:
+            if key in d.keys():
+                res1.extend([key] * d[key])
+        for key in d.keys():
+            if key not in arr2:
+                res2.extend([key] * d[key])
+        if len(res2) > 0:
+            res2.sort()
+            arr1 = res1 + res2
+        elif len(res2) == 0:
+            arr1 = res1
+        return arr1
