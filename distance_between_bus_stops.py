2f2b037266cfddf16893aacb5a74f4b9b0757f9c
Iliyan Galabov <milenov556@gmail.com>
2025-05-19 12:17:36 +0300

    Update distance_between_bus_stops.py

--- distance_between_bus_stops.py
+++ distance_between_bus_stops.py
@@ -4,7 +4,10 @@ from typing import List
 class Solution:
     def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
 
-        result = min(sum(distance[start:destination]), sum(distance[:start]) + sum(distance[destination:]))
+        if start < destination:
+            result = min(sum(distance[start:destination]), sum(distance[:start]) + sum(distance[destination:]))
+        else:
+            result = min(sum(distance[destination:start]), sum(distance[start:]) + sum(distance[:destination]))
 
         return result
-      
+        
