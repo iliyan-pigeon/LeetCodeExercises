e2813e96f9e47f89e3c225ccf7a0d4dbdc3f6005
Iliyan Galabov <milenov556@gmail.com>
2025-05-20 17:20:07 +0300

    Update longest_palindrome.py

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -5,15 +5,13 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         count = Counter(s)
 
-        odd_counts = 0
+        odd_added = 0
         length = 0
 
         for ch, amount in count.items():
-            if amount % 2 != 0 and odd_counts == 0:
-                odd_counts += 1
-                length += amount
+            if amount % 2 != 0 and odd_added < amount:
+                odd_added = amount
             elif amount % 2 == 0:
                 length += amount
 
-        return length
-      
+        return length + odd_added
