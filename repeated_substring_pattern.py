8ca2a99ee453bd138ce25baa011ccb6fbba4b414
Iliyan Galabov <milenov556@gmail.com>
2025-05-22 09:56:10 +0300

    Update repeated_substring_pattern.py

--- repeated_substring_pattern.py
+++ repeated_substring_pattern.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         s_copy = s
@@ -11,4 +12,14 @@ class Solution:
                 return True
 
         return False
-      
+
+
+# Solution 2
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+
+        if not str:
+            return False
+
+        ss = (s+s)[1:-1]
+        return ss.find(s) != -1
