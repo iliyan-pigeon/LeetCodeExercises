41e55bc3e64d11f7e01a5632424aad1ef50a6a9f
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-05-22 10:36:30 +0300

    update

--- license_key_formatting.py
+++ license_key_formatting.py
@@ -1,7 +1,8 @@
+# Solution 1
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
         result = []
-        s = reversed([i.upper() if i.isalpha() else i for i in s.replace("-", "")])
+        s = reversed(s.replace('-', '').upper())
 
         current = []
         for i in s:
@@ -11,5 +12,22 @@ class Solution:
                 result.insert(0, "".join(current))
                 current = []
 
+        if current:
+            result.insert(0, "".join(current))
+
         return "-".join(result)
-      
+
+
+# Solution 2
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        s = s.replace('-', '').upper()
+        n = len(s)
+
+        first_group = n % k or k
+        res = [s[:first_group]]
+
+        for i in range(first_group, n, k):
+            res.append(s[i:i + k])
+            
+        return '-'.join(res)
