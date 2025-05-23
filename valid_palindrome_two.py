7fa6af48f3ff854c7c405b14baf21e794af18736
Iliyan Galabov <milenov556@gmail.com>
2025-05-23 12:35:48 +0300

    Update valid_palindrome_two.py

--- valid_palindrome_two.py
+++ valid_palindrome_two.py
@@ -1,3 +1,26 @@
+# Solution 1
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if s == s[::-1]:
+            return True
+
+        first_pointer = 0
+        second_pointer = len(s)-1
+
+        while first_pointer <= len(s) // 2:
+            first_current = s[:first_pointer]+s[first_pointer+1:]
+            second_current = s[:second_pointer]+s[second_pointer+1:]
+
+            if first_current == first_current[::-1] or second_current == second_current[::-1]:
+                return True
+
+            first_pointer += 1
+            second_pointer -= 1
+
+        return False
+
+
+# Solution 2
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         if s == s[::-1]:
