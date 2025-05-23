23dd38441a27a5de9a93d9335a03f35839eb2291
Iliyan Galabov <milenov556@gmail.com>
2025-05-23 14:58:17 +0300

    Update valid_palindrome_two.py

--- valid_palindrome_two.py
+++ valid_palindrome_two.py
@@ -1,32 +1,20 @@
-# Solution1
+# Solution 1
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        if s == s[::-1]:
-            return True
+        def is_palindrome_range(i, j):
+            return all(s[k] == s[j - k + i] for k in range(i, j))
 
         first_pointer = 0
-        second_pointer = len(s)-1
-        third_pointer = len(s)//2
-        fourth_pointer = len(s)//2
-
-        while first_pointer <= len(s) // 2:
-            first_current = s[:first_pointer]+s[first_pointer+1:]
-            second_current = s[:second_pointer]+s[second_pointer+1:]
-            third_current = s[:third_pointer]+s[third_pointer+1:]
-            fourth_current = s[:fourth_pointer] + s[fourth_pointer + 1:]
-
-            if first_current == first_current[::-1] or second_current == second_current[::-1]:
-                return True
-
-            if third_current == third_current[::-1] or fourth_current == first_current[::-1]:
-                return True
-
+        second_pointer = len(s) - 1
+        
+        while first_pointer < second_pointer:
+            if s[first_pointer] != s[second_pointer]:
+                # Check by removing one character either from the start or end
+                return is_palindrome_range(first_pointer + 1, second_pointer) or is_palindrome_range(first_pointer, second_pointer - 1)
             first_pointer += 1
             second_pointer -= 1
-            third_current += 1
-            fourth_current -= 1
-
-        return False
+        
+        return True
 
 
 # Solution 2
@@ -37,6 +25,37 @@ class Solution:
 
         first_pointer = 0
         second_pointer = len(s)-1
+        third_pointer = len(s)//2
+        fourth_pointer = len(s)//2
+
+        while first_pointer <= fourth_pointer:
+            first_current = s[:first_pointer]+s[first_pointer+1:]
+            second_current = s[:second_pointer]+s[second_pointer+1:]
+            third_current = s[:third_pointer]+s[third_pointer+1:]
+            fourth_current = s[:fourth_pointer] + s[fourth_pointer + 1:]
+
+            if first_current == first_current[::-1] or second_current == second_current[::-1]:
+                return True
+
+            if third_current == third_current[::-1] or fourth_current == forth_current[::-1]:
+                return True
+
+            first_pointer += 1
+            second_pointer -= 1
+            third_pointer += 1
+            fourth_pointer -= 1
+
+        return False
+
+
+# Solution 3
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if s == s[::-1]:
+            return True
+
+        first_pointer = 0
+        second_pointer = len(s)-1
 
         while first_pointer <= len(s) // 2:
             first_current = s[:first_pointer]+s[first_pointer+1:]
@@ -51,7 +70,7 @@ class Solution:
         return False
 
 
-# Solution 3
+# Solution 4
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         if s == s[::-1]:
