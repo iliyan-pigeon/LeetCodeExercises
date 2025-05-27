180070e735e06e6622159bdd7878d6d10eace73d
Iliyan Galabov <milenov556@gmail.com>
2025-05-27 14:57:25 +0300

    Update buddy_strings.py

--- buddy_strings.py
+++ buddy_strings.py
@@ -1,27 +1,15 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
 
-        first = None
-        second = None
-
         if len(s) != len(goal):
             return False
 
         if s == goal:
-            return len(set(s)) < len(goal)
+            return len(set(s)) < len(s)
 
-        for i in goal:
-            if first is None and i in s:
-                first = s.index(i)
-            elif i in s:
-                second = i.index(i)
+        pairs = []
+        for i in range(len(s)):
+            if s[i] != goal[i]:
+                pairs.append((s[i], goal[i]))
 
-                if first > second:
-                    second, first = first, second
-
-                s_new = s[:first]+s[second]+s[first+1:second]+s[first]+s[second+1:]
-
-                if s_new == goal:
-                    return True
-
-        return False
+        return len(pairs) == 2 and pairs[0] == pairs[1][::-1]
