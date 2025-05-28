85cc04cf5a34beadd6bc3ce902a248481b018a7a
Iliyan Galabov <milenov556@gmail.com>
2025-05-28 16:40:44 +0300

    Create remove_outermost_parentheses.py

---
+++ remove_outermost_parentheses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def removeOuterParantheses(self, s: str) -> str:
+        mapping = {}
+        score = 0
+
+        if s == "":
+            return ""
+
+        for i, v in enumerate(s):
+            mapping[i] = score
+
+            if v == "(":
+                score += 1
+            elif v == ")":
+                score -= 1
+
+        first, second = sorted(set([i for i in mapping.values()]))[:2]
+        result = ""
+
+        for i in range(len(s)):
+            if mapping[i] == first and s[i] == "(":
+                continue
+            elif mapping[i] == second and s[i] == ")":
+                continue
+
+            result += s[i]
+
+        return result
