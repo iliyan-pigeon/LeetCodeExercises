4b7369830de08727e0584736c9a8328fd51786bd
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-05-28 15:18:30 +0300

    update

--- long_pressed_name.py
+++ long_pressed_name.py
@@ -1,6 +1,9 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
 
+        if name[0] != typed[0]:
+            return False
+
         valid = True
         t_index = 0
         for i, v in enumerate(name):
@@ -15,12 +18,16 @@ class Solution:
                     if v == typed[t_index]:
                         valid = True
                         break
+                    elif typed[t_index] != typed[t_index-1]:
+                        return False
             if not valid:
                 return False
 
             t_index += 1
 
-        if t_index <= len(typed)-1:
-            return False
+        while t_index <= len(typed)-1:
+            if typed[t_index] != name[-1]:
+                return False
+            t_index += 1
 
         return True
