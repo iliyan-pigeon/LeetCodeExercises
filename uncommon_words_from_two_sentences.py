ce11f3dce99d05ebbf8be7a7558f212a1658e840
Iliyan Galabov <milenov556@gmail.com>
2025-05-28 11:22:12 +0300

    Update uncommon_words_from_two_sentences.py

--- uncommon_words_from_two_sentences.py
+++ uncommon_words_from_two_sentences.py
@@ -1,10 +1,11 @@
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        s1 = s1.split(" ")
-        s2 = s2.split()
+        s1 = Counter(s1.split(" "))
+        s2 = Counter(s2.split(" "))
         combined = s1 + s2
 
-        return [i for i in combined if i not in s1 or i not in s2]
+        return [i for i in combined if i not in s1 and combined[i] == 1 or i not in s2 and combined[i] == 1]
