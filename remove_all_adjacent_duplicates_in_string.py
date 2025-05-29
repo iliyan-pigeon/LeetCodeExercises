4c4badf22f302b990a3ab2bbf13988543efc51a7
Iliyan Galabov <milenov556@gmail.com>
2025-05-29 16:43:57 +0300

    Update remove_all_adjacent_duplicates_in_string.py

--- remove_all_adjacent_duplicates_in_string.py
+++ remove_all_adjacent_duplicates_in_string.py
@@ -1,14 +1,12 @@
-from collections import Counter
-
-
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        amounts = Counter(s)
-
-        for ch, count in amounts.items():
-            if count % 2 != 0 and count != 1:
-                s = s.replace(ch, "", count-1)
-            elif count % 2 == 0:
-                s = s.replace(ch, "", count)
+        finished = False
+        while not finished:
+            for i in range(len(s)-1):
+                if s[i] == s[i+1]:
+                    s = s[:i]+s[i+2:]
+                    break
+            else:
+                finished = True
 
         return s
