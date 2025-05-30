3bc3a1f52c70c9de8f32e029ee06e581dd519537
Iliyan Galabov <milenov556@gmail.com>
2025-05-30 14:59:33 +0300

    Create day_of_the_year.py

---
+++ day_of_the_year.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dayOfYear(self, date: str) -> int:
+        year, month, day = date.split("-")
+
+        months = {"01": 0, "02": 31, "03": 59, "04": 90, "05": 120, "06": 151, "07": 181, "08": 212, "09": 243,
+                  "10": 273, "11": 304, "12": 334}
+
+        result = int(day) + months[month]
+
+        if int(year) % 400 == 0:
+            result += 1
+
+        return result
