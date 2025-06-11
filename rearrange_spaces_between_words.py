a8fa2cbf70f4c6c2ac312a1ce6ba71062d672937
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-06-11 11:47:35 +0300

    update

--- rearrange_spaces_between_words.py
+++ rearrange_spaces_between_words.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution:
     def reorderSpaces(self, text: str) -> str:
         words = []
@@ -17,7 +18,23 @@ class Solution:
         if current_word != "":
             words.append(current_word)
 
-        between = spaces // (len(words)-1)
-        after = spaces % (len(words)-1)
+        if len(words) > 1:
+            between = spaces // (len(words)-1) if spaces else 0
+            after = spaces % (len(words)-1) if spaces else 0
+        else:
+            between = 0
+            after = spaces
 
         return f"{' '*between}".join(words) + " "*after
+
+
+# Solution 2
+class Solution:
+    def reorderSpaces(self, text: str) -> str:
+        words = text.split()
+        cnt = len(words)
+        spaces = text.count(' ')
+        gap = 0 if cnt == 1 else spaces // (cnt - 1)
+
+        trailing_spaces = spaces - gap * (cnt - 1)
+        return (' ' * gap).join(words) + ' ' * trailing_spaces
