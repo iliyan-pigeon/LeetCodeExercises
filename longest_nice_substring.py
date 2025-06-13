e59762a16a827f858712bcd5623420ceaa61d027
Iliyan Galabov <milenov556@gmail.com>
2025-06-13 14:16:02 +0300

    Create longest_nice_substring.py

---
+++ longest_nice_substring.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        substrings = [s]
+        results = []
+
+        while substrings:
+            for index, sb in enumerate(substrings):
+                for i, ch in enumerate(sb):
+                    if ch.isupper():
+                        if chr(ord(ch)+32) not in sb:
+                            substrings.pop(index)
+
+                            substrings.append(sb[:i])
+                            substrings.append(sb[i+1:])
+                            break
+                    elif ch.islower():
+                        if chr(ord(ch)-32) not in sb:
+                            substrings.pop(index)
+
+                            substrings.append(sb[:i])
+                            substrings.append(sb[i + 1:])
+                            break
+                else:
+                    if sb not in results and sb != "":
+                        results.append(sb)
+                    else:
+                        substrings.pop(index)
+
+        result = sorted(results, key=len)[0] if results else ""
+
+        return result
