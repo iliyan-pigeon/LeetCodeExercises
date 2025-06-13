bbe1ad482d7fb3cf60c568eb548168e400a8d1f2
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-06-13 15:49:29 +0300

    update

--- check_if_one_string_swap_can_make_strings_equal.py
+++ check_if_one_string_swap_can_make_strings_equal.py
@@ -4,13 +4,16 @@ class Solution:
         if len(s1) != len(s2):
             return False
 
+        if s1 == s2:
+            return True
+
         diff_indexes = []
 
         for i in range(len(s1)):
             if s1[i] != s2[i]:
                 diff_indexes.append(i)
 
-        if len(diff_indexes) not in [0, 2]:
+        if len(diff_indexes) != 2:
             return False
 
         if s1[diff_indexes[0]] == s2[diff_indexes[1]] and s1[diff_indexes[1]] == s2[diff_indexes[0]]:
