09bb1c40e257b20afb750ed9b08480d025ed09d9
Iliyan Galabov <milenov556@gmail.com>
2025-06-13 08:40:41 +0300

    Update latest_time_by_replacing_hidden_digits.py

--- latest_time_by_replacing_hidden_digits.py
+++ latest_time_by_replacing_hidden_digits.py
@@ -1,18 +1,21 @@
-from random import randint
-
-
 class Solution:
     def maximumTime(self, time: str) -> str:
 
         for i, v in enumerate(time):
             if v == "?":
                 if i == 0:
-                    time = time.replace(v, str(randint(0, 2)), 1)
+                    if int(time[i+1]) <= 4:
+                        time = time.replace(v, "2", 1)
+                    else:
+                        time = time.replace(v, "9", 1)
                 elif i == 1:
-                    time = time.replace(v, str(randint(0, 3)), 1)
+                    if int(time[i-1]) == 2:
+                        time = time.replace(v, "4", 1)
+                    else:
+                        time = time.replace(v, "9", 1)
                 elif i == 3:
-                    time = time.replace(v, str(randint(0, 5)), 1)
+                    time = time.replace(v, "5", 1)
                 elif i == 4:
-                    time = time.replace(v, str(randint(0, 9)), 1)
+                    time = time.replace(v, "9", 1)
 
         return time
