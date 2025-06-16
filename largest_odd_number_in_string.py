7c789852fd4a571ff6176900e6512ae6d03fb6cd
Iliyan Galabov <milenov556@gmail.com>
2025-06-16 15:43:10 +0300

    Update largest_odd_number_in_string.py

--- largest_odd_number_in_string.py
+++ largest_odd_number_in_string.py
@@ -1,14 +1,14 @@
 class Solution:
-    def largest_odd_number_in_string(self, num: str) -> str:
+    def largestOddNumber(self, num: str) -> str:
         odd_number = False
 
         while not odd_number:
+            if num == "":
+                return ""
+            
             if int(num) % 2 != 0:
                 odd_number = num
 
             num = num[:-1]
-
-            if num == "":
-                return ""
 
         return odd_number
