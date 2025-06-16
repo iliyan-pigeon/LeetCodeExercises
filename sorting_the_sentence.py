b76cacc4586583d6dcb2634ace10e12b7cbd450c
Iliyan Galabov <milenov556@gmail.com>
2025-06-16 10:36:44 +0300

    Create sorting_the_sentence.py

---
+++ sorting_the_sentence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        s = s.split(" ")
+        result = [0] * len(s)
+
+        for ch in s:
+            index = int(ch[-1]) -1
+            result[index] = ch[:-1]
+
+        return result
