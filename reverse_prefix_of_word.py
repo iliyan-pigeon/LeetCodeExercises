80a483ae41aa166972199bfc46216faca10af862
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-06-17 15:32:57 +0300

    update

--- reverse_prefix_of_word.py
+++ reverse_prefix_of_word.py
@@ -1,7 +1,10 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
+        if ch not in word:
+            return word
+
         index = word.index(ch)
 
-        word = word[:index+1][::-1] + word[index+1:]
+        word = word[:index + 1][::-1] + word[index + 1:]
 
         return word
