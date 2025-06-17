26f3ec5d3164b5d85d13fb80e94d0088b3ad60f7
Iliyan Galabov <milenov556@gmail.com>
2025-06-17 15:37:24 +0300

    Create final_value_of_variable_after_performing_operations.py

---
+++ final_value_of_variable_after_performing_operations.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        result = 0
+
+        for operation in operations:
+            if "+" in operation:
+                result += 1
+            elif "-" in operation:
+                result -= 1
+
+        return result
