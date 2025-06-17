1e0482d17578b492ddc6661576c6c42ad07b8297
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-06-17 15:55:29 +0300

    update

--- minimum_moves_to_convert_string.py
+++ minimum_moves_to_convert_string.py
@@ -1,13 +1,13 @@
 class Solution:
     def minimumMoves(self, s: str) -> int:
         result = 0
-        should_skip = 2
+        should_skip = 0
 
         for ch in s:
-            if should_skip == 0:
-                continue
-
-            if ch == "X":
+            if should_skip > 0:
+                should_skip -= 1
+            elif ch == "X":
                 result += 1
+                should_skip = 2
 
         return result
