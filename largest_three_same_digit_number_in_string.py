68b4eebd79eea3a5d7c142f3fada12641eaec2c5
Iliyan Galabov <milenov556@gmail.com>
2025-06-20 09:29:43 +0300

    Create largest_three_same_digit_number_in_string.py

---
+++ largest_three_same_digit_number_in_string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        result = None
+        current = ""
+
+        for ch in num:
+            if not current:
+                current += ch
+            elif ch == current[0]:
+                current += ch
+            else:
+                current = ch
+
+            if len(current) == 3:
+                if result is None:
+                    result = current
+                    current = ""
+                elif int(result) < int(current):
+                    result = current
+                    current = ""
+
+        return result
