a5b0c7432ca110cb78d01947d57060034cd419d6
Iliyan Galabov <milenov556@gmail.com>
2025-06-24 15:30:51 +0300

    Update odd_string_difference.py

--- odd_string_difference.py
+++ odd_string_difference.py
@@ -7,7 +7,11 @@ class Solution:
         converted = {}
 
         for word in words:
-            current = str([(ord(word[1])-97)-(ord(word[0])-97), (ord(word[2])-97)-(ord(word[1])-97)])
+            current = []
+            for i in range(len(word)-1):
+                current.append([(ord(word[i+1]) - 97) - (ord(word[i]) - 97), (ord(word[2]) - 97) - (ord(word[1]) - 97)])
+
+            current = str(current)
             if current not in converted:
                 converted[current] = []
             converted[current].append(word)
