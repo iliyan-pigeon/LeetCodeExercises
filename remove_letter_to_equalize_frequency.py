443089ee1d204612a200f0a778d72287e26016e5
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-06-24 11:36:02 +0300

    update

--- remove_letter_to_equalize_frequency.py
+++ remove_letter_to_equalize_frequency.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(word)):
             current = word[:i]+word[i+1:]
 
-            if set([i for i in Counter(current).values()]) == 1:
+            if len(set([i for i in Counter(current).values()])) == 1:
                 return True
 
         return False
