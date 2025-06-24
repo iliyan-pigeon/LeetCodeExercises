2b5d61107ac17352541586a973fa4cdd11806ba9
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-06-24 15:32:35 +0300

    update

--- odd_string_difference.py
+++ odd_string_difference.py
@@ -9,7 +9,7 @@ class Solution:
         for word in words:
             current = []
             for i in range(len(word)-1):
-                current.append([(ord(word[i+1]) - 97) - (ord(word[i]) - 97), (ord(word[2]) - 97) - (ord(word[1]) - 97)])
+                current.append([(ord(word[i+1]) - 97) - (ord(word[i]) - 97)])
 
             current = str(current)
             if current not in converted:
