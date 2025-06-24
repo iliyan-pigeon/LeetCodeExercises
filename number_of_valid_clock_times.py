e1857c9622127ae8cb310636159c65b5227b1c2d
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-06-24 13:54:56 +0300

    update

--- number_of_valid_clock_times.py
+++ number_of_valid_clock_times.py
@@ -1,25 +1,25 @@
 class Solution:
     def countTime(self, time: str) -> int:
-        valid_times = 0
+        valid_times = 1
         hours, minutes = time.split(":")
 
         if hours[0] == "?" and hours[1] == "?":
-            valid_times += 24
+            valid_times *= 24
         elif hours[0] == "?":
-            if hours[1] <= 4:
-                valid_times += 3
+            if int(hours[1]) < 4:
+                valid_times *= 3
             else:
-                valid_times += 1
+                valid_times *= 2
         elif hours[1] == "?":
-            if hours[0] == 2:
-                valid_times += 5
+            if int(hours[0]) == 2:
+                valid_times *= 4
             else:
-                valid_times += 10
+                valid_times *= 10
 
         if minutes[0] == "?" and minutes[1] == "?":
             valid_times *= 60
         elif minutes[0] == "?":
-            valid_times *= 7
+            valid_times *= 6
         elif minutes[1] == "?":
             valid_times *= 10
 
