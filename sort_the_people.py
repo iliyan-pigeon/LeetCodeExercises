b60647c2a8df793cc8652e2c60a5851cbab14dd8
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-06-24 09:32:54 +0300

    update

--- sort_the_people.py
+++ sort_the_people.py
@@ -1,7 +1,10 @@
+from typing import List
+
+
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        the_dict = {names[i]: heights[i] for i in range(len(names))}
+        the_dict = {heights[i]: names[i] for i in range(len(names))}
 
-        result = [k for k, v in sorted(the_dict.items(), key=lambda item: item[1], reverse=True)]
+        result = [v for k, v in sorted(the_dict.items(), key=lambda item: item[0], reverse=True)]
 
         return result
