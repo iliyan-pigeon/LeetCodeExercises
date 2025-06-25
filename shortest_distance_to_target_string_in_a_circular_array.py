498411c9cd99fca973b183dedf66237902d2c5be
Iliyan Galabov <milenov556@gmail.com>
2025-06-25 14:14:56 +0300

    Create shortest_distance_to_target_string_in_a_circular_array.py

---
+++ shortest_distance_to_target_string_in_a_circular_array.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
+        shortest_path = 0
+
+        pointer_left = startIndex
+        pointer_right = startIndex
+
+        left_finished = False
+        right_finished = False
+
+        while not left_finished and not right_finished:
+            if words[pointer_left] == target or words[pointer_right] == target:
+                return shortest_path
+
+            pointer_left -= 1
+            pointer_right += 1
+            shortest_path += 1
+
+            if pointer_left == -1:
+                pointer_left = len(words)-1
+
+            if pointer_right == len(words):
+                pointer_right = 0
+
+            if pointer_left == startIndex:
+                left_finished = True
+            if pointer_right == startIndex:
+                right_finished = True
+
+        return -1
