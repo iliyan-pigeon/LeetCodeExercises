50d10571ccc2aa7b9028a242ed3d12dd29420f87
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-06-27 14:00:27 +0300

    update

--- find_words_containing_character.py
+++ find_words_containing_character.py
@@ -4,4 +4,3 @@ from typing import List
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         return [i for i in range(len(words)) if x in words[i]]
-    
--- number_of_changing_keys.py
+++ number_of_changing_keys.py
@@ -1,8 +1,9 @@
 class Solution:
     def countKeyChanges(self, s: str) -> int:
         key_changes = 0
+        s = s.lower()
 
-        for i in range(1, len(s.lower())):
+        for i in range(1, len(s)):
             if s[i] != s[i-1]:
                 key_changes += 1
 
