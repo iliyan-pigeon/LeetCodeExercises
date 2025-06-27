6712d8b9d82e823e35464d2689a1f5189996f230
Iliyan Galabov <milenov556@gmail.com>
2025-06-27 10:00:52 +0300

    Create make_three_strings_equal.py

---
+++ make_three_strings_equal.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMinimumOprations(self, s1: str, s2: str, s3: str) -> int:
+        min_length = min(len(s1), len(s2), len(s3))
+
+        operations = 0
+
+        while len(s1) > min_length or len(s2) > min_length or len(s3) > min_length:
+            if len(s1) > min_length:
+                s1 = s1[:-1]
+                operations += 1
+            if len(s2) > min_length:
+                s2 = s2[:-1]
+                operations += 1
+            if len(s3) > min_length:
+                s3 = s3[:-1]
+                operations += 1
+
+        return operations if s1 == s2 == s3 else -1
