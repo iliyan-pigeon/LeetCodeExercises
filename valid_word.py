fedad4e46988e69c4417ad8c4de2f5ccf5a1b324
Iliyan Galabov <milenov556@gmail.com>
2025-07-07 14:20:45 +0300

    Update valid_word.py

--- valid_word.py
+++ valid_word.py
@@ -1,3 +1,22 @@
 class Solution:
     def isValid(self, word: str) -> bool:
-        pass
+        if len(word) < 3:
+            return False
+
+        if not word.isalnum():
+            return False
+
+        vowel = False
+        constant = False
+
+        for ch in word:
+            if ch in "aeiou" and ch.isalpha():
+                vowel = True
+
+            elif ch not in "aeiou" and ch.isalpha():
+                constant = True
+
+            if constant and vowel:
+                break
+
+        return vowel is True and constant is True
