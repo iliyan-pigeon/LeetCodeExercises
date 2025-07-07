eb23541b9008f4c71164c9ef99c7de1040561173
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-07-07 14:07:59 +0300

    update

--- count_the_number_of_special_characters_two.py
+++ count_the_number_of_special_characters_two.py
@@ -3,7 +3,7 @@ class Solution:
         special_chars = 0
 
         for ch in set(word):
-            if ch.islower() and ch.upper() in word and ch not in word[word.index(ch):]:
+            if ch.islower() and ch.upper() in word and ch not in word[word.index(ch.upper()):]:
                 special_chars += 1
 
         return special_chars
