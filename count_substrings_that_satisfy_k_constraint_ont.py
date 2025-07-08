25958e666e147cf03921dbcbfe02699cf93833b5
Iliyan Galabov <milenov556@gmail.com>
2025-07-08 15:36:25 +0300

    Create count_substrings_that_satisfy_k_constraint_ont.py

---
+++ count_substrings_that_satisfy_k_constraint_ont.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countKConstraintSubstrings(self, s: str, k: int) -> int:
+        sub_length = 1
+        result = 0
+
+        while sub_length < len(s)-1:
+
+            for i in range(len(s)+1-sub_length):
+                current = s[i:i+k]
+                if current.count("0") <= k or current.count("1") <= k:
+                    result += 1
+
+            sub_length += 1
+
+        return result
