e853b52787c28adc69a850063e6e597b8f64fa77
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-07-08 11:07:05 +0300

    update

--- find_the_encrypted_string.py
+++ find_the_encrypted_string.py
@@ -1,3 +1,4 @@
+# Solution
 class Solution:
     def getEncryptedString(self, s: str, k: int) -> str:
         encrypted_string = ""
@@ -5,9 +6,18 @@ class Solution:
         for i in range(len(s)):
             index = i + k
 
-            if index > len(s)-1:
+            while index > len(s) - 1:
                 index -= len(s)
 
             encrypted_string += s[index]
 
         return encrypted_string
+
+
+# Solution 2
+class Solution:
+    def getEncryptedString(self, s: str, k: int) -> str:
+        result = ""
+        for i in range(len(s)):
+            result += s[(i + k) % len(s)]
+        return result
