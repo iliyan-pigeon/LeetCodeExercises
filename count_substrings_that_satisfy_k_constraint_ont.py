377adb8b52d1af7e82131ef77fe1fbc35ff22644
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-07-08 15:43:04 +0300

    update

--- count_substrings_that_satisfy_k_constraint_ont.py
+++ count_substrings_that_satisfy_k_constraint_ont.py
@@ -3,10 +3,10 @@ class Solution:
         sub_length = 1
         result = 0
 
-        while sub_length < len(s)-1:
+        while sub_length < len(s)+1:
 
             for i in range(len(s)+1-sub_length):
-                current = s[i:i+k]
+                current = s[i:i+sub_length]
                 if current.count("0") <= k or current.count("1") <= k:
                     result += 1
 
