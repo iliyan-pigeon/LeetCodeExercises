ba14ebd86cce8b4795053190bafe42594fdd6978
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-07-09 15:33:40 +0300

    update

--- minimum_deletions_for_at_most_k_distinct_characters.py
+++ minimum_deletions_for_at_most_k_distinct_characters.py
@@ -1,10 +1,11 @@
+from collections import Counter
+
+
 class Solution:
     def minDeletion(self, s: str, k: int) -> int:
         s = Counter(s)
 
-        distinct = len([i for i in s.values() if i == 1])
-
-        if distinct <= 2:
+        if len(s) <= k:
             return 0
 
-        return distinct - 2
+        return sum(sorted([i for i in s.values()])[:len(s)-k])    
