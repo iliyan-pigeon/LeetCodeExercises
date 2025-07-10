9a713176da2429516a6da7503f65c8ac0a53eb48
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-07-10 14:27:20 +0300

    update

--- string_to_integer_atoi.py
+++ string_to_integer_atoi.py
@@ -4,11 +4,12 @@ class Solution:
         digit_appeared = False
         digit = ""
 
-        for ch in s:
+        for ch in s.strip():
             if digit_appeared and ch == "-":
                 break
             elif ch == "-":
                 sign = "-"
+                continue
             elif not ch.isnumeric():
                 break
             elif not digit_appeared and ch.isnumeric():
@@ -20,6 +21,11 @@ class Solution:
             return 0
 
         if sign == "-":
+            if -int(digit) < -(2**31):
+                return -(2**31)
+
             return -int(digit)
 
+        if int(digit) > 2**31-1:
+                return 2**31-1
         return int(digit)
