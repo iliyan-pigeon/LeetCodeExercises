01ec422fbb8839f14ad019f0c9593f504eea3f74
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-07-10 14:06:58 +0300

    update

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -1,7 +1,10 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
-        for i in range(len(s)-1, -1, -1):
-            for j in range(len(s)-i):
+        if len(s) == 1:
+            return s
+
+        for i in range(len(s), -1, -1):
+            for j in range(len(s)-i+1):
                 if s[j:j+i] == s[j:j+i][::-1]:
                     return s[j:j+i]
