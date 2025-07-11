ceea36c3a7f627ec7032b78c207130ef67e8ade5
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-07-11 10:46:42 +0300

    update

--- letter_combinations_of_a_phone_number.py
+++ letter_combinations_of_a_phone_number.py
@@ -3,6 +3,9 @@ class Solution:
         mapping = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
         combinations = []
 
+        if digits == "":
+            return []
+
         def backtrack(index, path):
 
             if index == len(digits):
