7d7f5d1d43904254fdcc0e146b899a8aca21eed1
Iliyan Galabov <milenov556@gmail.com>
2025-07-14 09:42:31 +0300

    Update generate_parentheses.py

--- generate_parentheses.py
+++ generate_parentheses.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        pass
+        def backtrack(s='', opened=0, closed=0):
+            if len(s) == 2 * n:
+                result.append(s)
+                return
+            if opened < n:
+                backtrack(s + '(', opened + 1, closed)
+            if closed < opened:
+                backtrack(s + ')', opened, closed + 1)
+
+        result = []
+        backtrack()
+        return result
