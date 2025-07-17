08ac3fd6fab8a51452a73e82f7a5668941f0b68f
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-07-17 10:27:20 +0300

    update

--- minimum_cost_to_move_chips_to_same_position.py
+++ minimum_cost_to_move_chips_to_same_position.py
@@ -3,4 +3,4 @@ from typing import List
 
 class Solution:
     def minCostToMoveChips(self, position: List[int]) -> int:
-        return min(len([i for i in range(len(position)) if i % 2 == 0]), len([i for i in range(len(position)) if i % 2 != 0]))
+        return min(len([i for i in position if i % 2 == 0]), len([i for i in position if i % 2 != 0]))
