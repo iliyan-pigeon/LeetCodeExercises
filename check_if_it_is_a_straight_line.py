d428a85131a41e8151ea29f96253967cb4741f49
Iliyan Galabov <milenov556@gmail.com>
2025-07-17 11:45:47 +0300

    Update check_if_it_is_a_straight_line.py

--- check_if_it_is_a_straight_line.py
+++ check_if_it_is_a_straight_line.py
@@ -6,10 +6,15 @@ class Solution:
 
         coordinates.sort()
         previous = coordinates[0]
+        first_distance = 0
+        second_distance = 0
 
         for i in range(1, len(coordinates)):
-            if coordinates[i][0] - previous[0] > 1 or \
-                    coordinates[i][1] - previous[1] > 1:
+            if i == 1:
+                first_distance = coordinates[i][0] - previous[0]
+                second_distance = coordinates[i][1] - previous[1]
+            if coordinates[i][0] - previous[0] > first_distance or \
+                    coordinates[i][1] - previous[1] > second_distance:
                 return False
             previous = coordinates[i]
 
