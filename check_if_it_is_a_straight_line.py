3c6d88e5096b2e8a666befe8bd3eac857223152c
Iliyan Galabov <milenov556@gmail.com>
2025-07-17 11:42:38 +0300

    Update check_if_it_is_a_straight_line.py

--- check_if_it_is_a_straight_line.py
+++ check_if_it_is_a_straight_line.py
@@ -3,7 +3,14 @@ from typing import List
 
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        if [i[0] for i in coordinates] == [i for i in range(coordinates[0][0], coordinates[-1][0]+1)] and \
-                [i[1] for i in coordinates] == [i for i in range(coordinates[0][1], coordinates[-1][1]+1)]:
-            return True
-        return False
+
+        coordinates.sort()
+        previous = coordinates[0]
+
+        for i in range(1, len(coordinates)):
+            if coordinates[i][0] - previous[0] > 1 or \
+                    coordinates[i][1] - previous[1] > 1:
+                return False
+            previous = coordinates[i]
+
+        return True
