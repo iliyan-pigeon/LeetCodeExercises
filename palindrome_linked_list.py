25be63c6d7d63dbe6040d6c6515da4ff208ba0d1
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-07-29 15:21:28 +0300

    update

---
+++ palindrome_linked_list.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        values = []
+
+        while head:
+            values.append(head.val)
+            head = head.next
+
+        return values == values[::-1]
+    
