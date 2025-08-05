96c4add3bd914f54326018a6cc270ad3cb9c73fc
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-08-05 10:00:35 +0300

    update

--- reorder_list.py
+++ reorder_list.py
@@ -62,3 +62,30 @@ class Solution:
         while second:
             first.next, first = second, first.next
             second.next, second = first, second.next
+
+
+# Solution 3
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return
+
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev, curr = None, slow.next
+        slow.next = None
+        while curr:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
