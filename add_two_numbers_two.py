1220d3bd2fd32d08d0d660871ca714fd75bc4cd6
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-08-07 15:58:21 +0300

    update

--- add_two_numbers_two.py
+++ add_two_numbers_two.py
@@ -7,6 +7,7 @@ class ListNode:
         self.next = next
 
 
+# Solution 1
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         l1_string = ""
@@ -31,4 +32,34 @@ class Solution:
             current = current.next
 
         return dummy.next
-    
+
+
+# Solution 2
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        reversed_l1 = self.reverse_nodes(l1)
+        reversed_l2 = self.reverse_nodes(l2)
+        carry = 0
+        dummy = ListNode(-1)
+        curr = dummy
+        while reversed_l1 or reversed_l2 or carry:
+            reversed_l1_val = reversed_l1.val if reversed_l1 else 0
+            reversed_l2_val = reversed_l2.val if reversed_l2 else 0
+            sums = reversed_l1_val + reversed_l2_val + carry
+            val, carry = sums % 10, sums // 10
+            to_add = ListNode(val)
+            curr.next = to_add
+            curr = curr.next
+            reversed_l1 = reversed_l1.next if reversed_l1 else None
+            reversed_l2 = reversed_l2.next if reversed_l2 else None
+        return self.reverse_nodes(dummy.next)
+
+    def reverse_nodes(self, head):
+        prev = None
+        curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
