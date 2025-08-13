135d511d6f499439bb97c02fbf14b695dc0d47ff
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-08-13 11:41:46 +0300

    update

--- same_tree.py
+++ same_tree.py
@@ -1,29 +1,42 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-class Solution(object):
+class Solution:
     def isSameTree(self, p, q):
-        result = True
-        p_front = p
-        q_front = q
-        p_back = p
-        q_back = q
 
-        while p_front and q_front and p_back and q_back:
-            if p_front.val != q_front.val:
-                result = False
-                break
-            elif p_back.val != q_back.val:
-                result = False
-                break
+        def check(p_node, q_node):
+            r = True
 
-            p_front = p_front.right
-            q_front = q_front.right
-            p_back = p_back.left
-            q_back = q_back.left
+            if not p_node and not q_node:
+                return r
+            elif not p_node:
+                return False
+            elif not q_node:
+                return False
 
+            if p_node.val != q_node.val:
+                r = False
+                return r
+
+            if p_node.left and q_node.left:
+                r = check(p_node.left, q_node.left)
+                if r is False:
+                    return r
+            elif not p_node.left and not q_node.left:
+                pass
+            else:
+                r = False
+                return r
+
+            if p_node.right and q_node.right:
+                r = check(p_node.right, q_node.right)
+                if r is False:
+                    return r
+            elif not p_node.right and not q_node.right:
+                pass
+            else:
+                r = False
+                return r
+
+            return r
+
+        result = check(p, q)
         return result
+    
