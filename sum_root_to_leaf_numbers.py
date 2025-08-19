8048b175c2f1ea2a2f27620538067bd64d62765d
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-08-19 16:11:23 +0300

    update

--- sum_root_to_leaf_numbers.py
+++ sum_root_to_leaf_numbers.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -8,4 +11,3 @@ class TreeNode:
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         pass
-        
