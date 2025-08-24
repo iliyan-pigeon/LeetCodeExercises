2a035cc7d1ad3841bcd42a83b26241de3b1029bd
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-08-24 14:26:16 +0300

    update

--- binary_tree_max_path_sum.py
+++ binary_tree_max_path_sum.py
@@ -1,5 +1,12 @@
 from typing import Optional
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
