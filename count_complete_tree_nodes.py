cdcd6d700daeb28c314d3668a9a7bfdc6cc4040e
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-09-06 08:49:05 +0300

    update

--- count_complete_tree_nodes.py
+++ count_complete_tree_nodes.py
@@ -1,7 +1,13 @@
 from typing import Optional
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
         pass
-        
