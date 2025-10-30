e0c3e3c183bfa5387c098259b1ae23ff6b15d729
iliyan-pigeon <milenov556@gmail.com>
2025-10-30 16:21:51 +0200

    update

--- remove_duplicates_from_sorted_array_two.py
+++ remove_duplicates_from_sorted_array_two.py
@@ -1,5 +1,22 @@
+from typing import List
+
+
 class Solution(object):
     def removeDuplicates(self, nums):
         for i in range(len(nums) - 1, -1, -1):
             if nums.count(nums[i]) > 2:
                 nums.pop(i)
+
+
+# Solution 2
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        pointer = 2
+
+        while pointer < len(nums):
+            if nums[pointer-1] == nums[pointer] and nums[pointer-2] == nums[pointer]:
+                nums.pop(pointer)
+            else:
+                pointer += 1
+
+        return len(nums)
