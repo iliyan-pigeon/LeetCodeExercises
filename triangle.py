ec4a91275041be9d50c575b7afe2f7c6298755f4
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-11-04 13:53:50 +0200

    update

--- triangle.py
+++ triangle.py
@@ -19,3 +19,13 @@ class Solution:
                     new_row[index+1].append(current_number)
 
             triangle[i+1] = [min(i) for i in new_row]
+
+
+# Solution 2
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+        return triangle[0][0]
+    
