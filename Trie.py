68bcb7e2111f552ce444439dae7bf2ba34afcc2d
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-11-13 09:15:50 +0200

    update

---
+++ Trie.py
@@ -0,0 +1,19 @@
+class Trie
+
+    def __init__(self):
+        self.trie = []
+
+    def insert(self, word: str) -> None:
+        self.trie.append(word)
+
+    def search(self, word: str) -> bool:
+        if word in self.trie:
+            return True
+        return False
+
+    def startsWitsh(self, prefix: str) -> bool:
+        for word in self.trie:
+            if word.startswith(prefix):
+                return True
+            return False
+        
