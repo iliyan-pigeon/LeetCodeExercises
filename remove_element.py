36f67e1d1029fb7d8c53bb10db6952c8bd9dafa0
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-11-20 11:25:36 +0200

    update

--- remove_element.py
+++ remove_element.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution(object):
     def removeElement(self, nums, val):
         
@@ -5,4 +8,13 @@ class Solution(object):
             nums.remove(val)
 
         return len(nums)
-      
+
+
+# Solution 2
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        removed = 0
+        for i in range(len(nums)):
+            if nums[i-removed] == val:
+                nums.pop(i-removed)
+                removed += 1
