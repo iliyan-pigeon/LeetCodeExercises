f098e7b7de8c5aa2f95c2a9120c99630c7e3d02b
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-11-20 10:53:00 +0200

    update

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -36,3 +36,30 @@ class Solution:
 
         nums1.sort()
 
+
+# Solution 3
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int):
+
+        index = len(nums1)-1
+
+        while m and n:
+            if nums1[m-1] >= nums2[n-1]:
+                nums1[index] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[index] = nums2[n-1]
+                n -= 1
+            index -= 1
+
+        while m:
+            nums1[index] = nums1[m - 1]
+            m -= 1
+            index -= 1
+
+        while n:
+            nums1[index] = nums2[n - 1]
+            n -= 1
+            index -= 1
+
+        return nums1
