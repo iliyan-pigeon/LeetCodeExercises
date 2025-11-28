fb4817b27e50ca9636a49d33b689d0f6721509b1
ILIYAN MILENOV GALABOV <milenov556@gmail.com>
2025-11-28 14:07:59 +0200

    update

--- jump_game.py
+++ jump_game.py
@@ -1,3 +1,7 @@
+# Solution 1
+from typing import List
+
+
 class Solution(object):
     def canJump(self, nums):
         farthest = 0
@@ -8,4 +12,23 @@ class Solution(object):
             if farthest >= len(nums) - 1:
                 return True
         return False
-        
+
+
+# Solution 2
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        length = len(nums)
+        if length == 1:
+            return True
+        answers = [False] * length
+
+        for i in range(len(nums)-1, -1, -1):
+            for j in range(1, nums[i]+1):
+                if i + j >= length-1:
+                    answers[i] = True
+                    break
+                elif answers[i+j] is True:
+                    answers[i] = True
+                    break
+
+        return answers[0]
